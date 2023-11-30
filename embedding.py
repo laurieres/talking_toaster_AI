113dc6aad1d3d8402772a7d56dca14a52eedbcd1
Arnaud de Voghel <arnaud.dv@hotmail.com>
2023-11-30 12:14:39 +0000

    embedding done

--- embedding.py
+++ embedding.py
@@ -2,16 +2,43 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI # Choosing which LLM
+from langchain.chains.question_answering import load_qa_chain
+import os
 
-api_key = "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"
 
-#fridge
-loader = PyPDFLoader("/Users/arnauddevoghel/code/arnaud2vo/talking_toaster_AI/datasets/manuals/fridge.pdf")
-data = loader.load()
+def communicate_with_manual(object_type, question):
+    api_key = os.environ.get("OPENAI_API_KEY")
+    manuals_folder = "datasets/manuals"
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
-texts = text_splitter.split_documents(data)
+    # Construct the path to the manual based on the object type
+    manual_path = os.path.join(manuals_folder, f"{object_type.lower()}.pdf")
 
-embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+    # Load the manual
+    loader = PyPDFLoader(manual_path)
+    data = loader.load()
 
-vector_db = Chroma.from_documents(texts, embeddings)
+    # Split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
+    texts = text_splitter.split_documents(data)
+
+    # Embed the text using OpenAI embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+    vector_db = Chroma.from_documents(texts, embeddings)
+
+    # Use OpenAI language model
+    llm = OpenAI(temperature=0, openai_api_key=api_key)
+    chain = load_qa_chain(llm, chain_type="map_reduce")
+
+    # Perform similarity search and run the chain
+    query = question
+    docs = vector_db.similarity_search(query)
+    result = chain.run(input_documents=docs, question=query)
+
+    return result
+
+# Example usage:
+object_type = "Refrigerator"
+question = "How do i make it work?"
+result = communicate_with_manual(object_type, question)
+print(result)
