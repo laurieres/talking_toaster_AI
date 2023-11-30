307d3363e9f22f4965c28b44adecdc1300adca27
Louis Aurieres <louis.aurieres@essec.edu>
2023-11-30 15:50:30 +0100

    Improving Streamlit App

--- streamlit_photo_test.py
+++ streamlit_photo_test.py
@@ -10,9 +10,41 @@ from yolov5_v28112023.classify.predict_laurieres import run
 
 # acceptable_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng']
 
-st.markdown("""# Welcome to Toaster Website 🍞""")
+st.set_page_config(layout="wide")
 
-img_file_buffer = st.camera_input("Take a picture")
+# Image at the top
+
+custom_html = """
+<div class="banner">
+    <img src="https://img.freepik.com/premium-photo/3d-illustration-yellow-honeycomb-monochrome-honeycomb-honey_116124-2277.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701302400&semt=ais">
+</div>
+<style>
+    .banner {
+        width: 120%;
+        height: 200px;
+        overflow: hidden;
+    }
+    .banner img {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+"""
+# Display the custom HTML
+st.components.v1.html(custom_html)
+
+# Displaying an image on the app
+
+#website_image = Image.open('yolov5_v28112023/toaster_streamlit.jpg')
+#st.image(website_image)
+
+# Running our model
+
+st.markdown("""# Welcome to Talking Toaster App 🍞🤖""")
+
+st.markdown("""### Please take of picture of your domestic appliance ☕️""")
+
+img_file_buffer = st.camera_input("")
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
@@ -23,19 +55,13 @@ if img_file_buffer is not None:
 
     img.save("camera.jpg")
 
-    res = run(source="camera.jpg")
+    image_pred = run(source="camera.jpg")
 
-    st.write(f"Your photo is {res[0]}")
+    st.markdown(f"Your photo is a {image_pred[0]}")
+    #st.write(f"Your photo is {image_pred[0]} with a probability of {round(image_pred[1].item(),2)}")
+
+else:
+    st.write(f"We were not able to upload your photo, please try again 🙌")
 
 
-
-#path_to_img_file_buffer = ''
-
-# Calling the model and make a predictions
-
-#image_pred = run(source='img_file_buffer')
-
-#if image_pred is not None:
-#    st.write(f"Your photo is {image_pred[0]} with a probability of {round(image_pred[1].item(),2)}")
-#else:
-#    st.write(f"We were not able to upload your photo, please try again 🙌")
+#st.sidebar.test_input('Name')
