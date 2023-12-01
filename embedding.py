2815e64e3160585bc9c026bd067575195c6e5796
Christoph Kuck <mail@christophkuck.com>
2023-12-01 12:59:11 +0000

    updated embedding and frontend

--- embedding.py
+++ embedding.py
@@ -1,16 +1,16 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI # Choosing which LLM
+from langchain.llms.openai import OpenAI # Choosing which LLM
 from langchain.chains.question_answering import load_qa_chain
 import os
 
 
 def embed_and_vectorize_pdf(object_type):
     #api_key = os.environ.get("OPENAI_API_KEY")
-    api_key = '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'
-    manuals_folder = "talking_toaster_AI/datasets/manuals"
+    api_key = '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'
+    manuals_folder = "datasets/"
 
     # Construct the path to the manual based on the object type
     manual_path = os.path.join(manuals_folder, f"{object_type.lower()}.pdf")
@@ -31,7 +31,7 @@ def embed_and_vectorize_pdf(object_type):
 
 def communicate_with_manual(vector_db, question):
     api_key = os.environ.get("OPENAI_API_KEY")
-
+    api_key = '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'
     # Use OpenAI language model
     llm = OpenAI(temperature=0, openai_api_key=api_key)
     chain = load_qa_chain(llm, chain_type="map_reduce")
@@ -45,7 +45,8 @@ def communicate_with_manual(vector_db, question):
 
 # Example usage:
 object_type = "toaster"
-question = "what age do i have to be to use you?"
+question = "What are the electrical properties?"
+print(os.getcwd())
 vector_db = embed_and_vectorize_pdf(object_type)
 result = communicate_with_manual(vector_db, question)
 print(result)
